d1000411dff8d5437ebfa4cd3ac73d15920725ee
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-06-21 17:17:22 +0700

    feat(integrations): add webpack

---
+++ packages/unplugin/src/webpack.ts
@@ -0,0 +1,11 @@
+import { webpack } from './index';
+
+/**
+ * Creates the CSSX adapter for webpack.
+ *
+ * @param options CSSX adapter options.
+ * @returns The webpack plugin.
+ */
+const cssxWebpack = webpack;
+
+export default cssxWebpack;
