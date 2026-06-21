3c0e291a9e62157685953afe98a21d3d2ad57c6c
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-06-21 11:42:22 +0700

    feat(integrations): add rspack

---
+++ packages/unplugin/src/rspack.ts
@@ -0,0 +1,11 @@
+import { rspack } from './index';
+
+/**
+ * Creates the CSSX adapter for Rspack.
+ *
+ * @param options CSSX adapter options.
+ * @returns The Rspack plugin.
+ */
+const cssxRspack = rspack;
+
+export default cssxRspack;
