2cfb4f14f97c911fb3acba4ed413868498ccf524
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-06-21 16:55:01 +0700

    feat(integrations): add vite

---
+++ packages/unplugin/src/vite.ts
@@ -0,0 +1,11 @@
+import { vite } from './index';
+
+/**
+ * Creates the CSSX adapter for Vite.
+ *
+ * @param options CSSX adapter options.
+ * @returns The Vite plugin.
+ */
+const cssxVite = vite;
+
+export default cssxVite;
