4f1b76744421668d2ab23ecf18ce38eab70a3914
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-06-21 10:38:35 +0700

    feat(integrations): add rollup

---
+++ packages/unplugin/src/rollup.ts
@@ -0,0 +1,11 @@
+import { rollup } from './index';
+
+/**
+ * Creates the CSSX adapter for Rollup.
+ *
+ * @param options CSSX adapter options.
+ * @returns The Rollup plugin.
+ */
+const cssxRollup = rollup;
+
+export default cssxRollup;
