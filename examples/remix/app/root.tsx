d834d80283c0a8873e404a3e75ff7b045e34fcde
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-07-10 23:17:34 +0700

    feat(examples): add root

---
+++ examples/remix/app/root.tsx
@@ -0,0 +1,27 @@
+import type { LinksFunction } from '@remix-run/node';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import appStyles from './app.css?url';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: '/cssx.css' },
+  { rel: 'stylesheet', href: appStyles },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
