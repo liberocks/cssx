d9832cc90884db220215d970264a00893f666609
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-09-13 20:13:00 +0700

    refactor: isolate css serialization

---
+++ packages/compiler/src/serialize-css.test.ts
@@ -0,0 +1,20 @@
+import { expect, it } from 'vitest';
+
+import type { CssxRule } from './cssx-rule';
+import { serializeCss } from './serialize-css';
+
+const rules = ([css]: readonly (readonly string[])[]): readonly CssxRule[] =>
+  css.map((value, index) => ({ className: `c${index}`, css: value }));
+
+it('deduplicates and sorts unique rule CSS', () => {
+  expect(serializeCss(rules([['.b{}', '.a{}', '.b{}', '.a{}']]))).toBe('.a{}.b{}');
+});
+
+it('wraps non-empty CSS in a CSS layer when requested', () => {
+  expect(serializeCss(rules([['.a{}', '.b{}']]), { layer: 'utilities' })).toBe('@layer utilities{.a{}.b{}}');
+});
+
+it('does not wrap empty CSS in a layer', () => {
+  expect(serializeCss([], { layer: 'utilities' })).toBe('');
+  expect(serializeCss(rules([['', '']]), { layer: 'utilities' })).toBe('');
+});
